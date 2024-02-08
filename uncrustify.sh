ed3ebd3afda349323733407ee693a138974642b7
Luca Ferrari <fluca1978@gmail.com>
2024-02-08 16:53:35 +0100

    [#398] Update uncrustify configuration for version 0.78
    Fixes the configuration of uncrustify for version 0.78.1 built from
    the GitHub repository.
    
    Also adds a few checks to the `uncrustify.sh` script to verify the
    program is there.
    
    See <https://github.com/pgmoneta/pgmoneta/commit/607a4a1f53a79c356b293d32941a8da1ee1a6f42>
    
    Close #398

--- uncrustify.sh
+++ uncrustify.sh
@@ -8,6 +8,20 @@ function indent()
     done
 }
 
+# check the executable and the configuration file
+# are there
+if [ -z $( which uncrustify 2>/dev/null ) ]; then
+    echo "Cannot find an \`uncrustify\` executable!"
+    exit 1
+fi
+
+if [ ! -r uncrustify.cfg ]; then
+    echo "Missing configuration file  \`uncrustify.cfg\`"
+    echo "(wrong directory?)"
+    exit 2
+fi
+
+
 indent "src/*.c"
 indent "src/include/*.h"
 indent "src/libpgagroal/*.c"
